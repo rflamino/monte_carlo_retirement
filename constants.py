77bf6946fdb8f1fa01f820d5df806efa06588bf0
Reinaldo S Flamino <reinaldosflamino@gmail.com>
2026-02-13 01:02:24 -0300

    refact #2

--- constants.py
+++ constants.py
@@ -1,11 +1,8 @@
-
-# constants.py
-
 MONTHS_PER_YEAR: int = 12
 DEFAULT_PLOT_FILENAME: str = "retirement_projection.png"
-MINIMUM_SIMULATIONS_FOR_SEARCH_STEP: int = 100 
-SMALL_EPSILON: float = 1e-6 
+MINIMUM_SIMULATIONS_FOR_SEARCH_STEP: int = 100
+SMALL_EPSILON: float = 1e-6
 
 # Plotting constants
-TEXT_INPUT_COLOR = '#1f77b4'
-TEXT_OUTPUT_COLOR = '#ff7f0e'
+TEXT_INPUT_COLOR = "#1f77b4"
+TEXT_OUTPUT_COLOR = "#ff7f0e"
